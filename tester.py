723f206e7560b9dcf855133dcfead7a98745a512
Tina <thuang45@g.ucla.edu>
2022-10-23 20:23:12 -0700

    test cases added by Tina Huang and Evan Zhong

--- tester.py
+++ tester.py
@@ -93,10 +93,11 @@ def generate_test_case_structure(cases, dir, category='', expect_failure=False, 
 
 def generate_test_suite(version):
   successes = {1,2,6,8,10,27,28}
-  fails = {1,7,9}
+  fails = {1,2,3,7,9}
   campuswire_tests = {1,2}
   youngs_tests = {1,2,3,4,5,6,7,8}
   qingyangs_tests = {1,2,3,4,5}
+  tina_evan_tests = {1,4,5,100,101,102,103,104,105}
   return generate_test_case_structure(
     successes,
     f'testsv{version}/',
@@ -127,6 +128,12 @@ def generate_test_suite(version):
     'Correctness',
     False,
     qingyangs_tests
+  ) + generate_test_case_structure(
+    tina_evan_tests,
+    f'tina_evan_testsv{version}/',
+    'Correctness',
+    False,
+    tina_evan_tests
   )
 
 # main entrypoint - just calls functions :)
